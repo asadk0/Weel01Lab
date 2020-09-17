48291f5d3c6e245a6d30400a1a6dae4154a262b6
759175 <759175@SAIT224378IT.ACDM.DS.SAIT.CA>
2020-09-17 15:25:15 -0600

    Redoing the assignment for practicing.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author 759175
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a test submission for Lab 01");
+    }
+    
+}
